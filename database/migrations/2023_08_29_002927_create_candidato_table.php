47f2871cdd9865a900b82935c33bdccbf3627c3b
Natalia Mello <rosbakmello@gmail.com>
2023-09-04 19:46:18 -0300

    feitas migrações

--- database/migrations/2023_08_29_002927_create_candidato_table.php
+++ database/migrations/2023_08_29_002927_create_candidato_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('candidato', function (Blueprint $table) {
             $table->id();
+            $table->string('cpf');
+            $table->string('nome');
+            $table->date('dataNasc');
+            $table->string('telefone');
+            $table->string('email');
+            $table->string('senha');
+            $table->string('genero');
+            $table->unsignedBigInteger('cidade_id');
             $table->timestamps();
+
+            $table->foreign('cidade_id')->references('id')->on('cidade');
         });
     }
 
