742e31c22e4aabf762e7eb12c226f67c4980ebd4
Natalia Mello <rosbakmello@gmail.com>
2023-10-02 19:46:54 -0300

    controllers corrigidos e salvando no banco

--- app/Http/Controllers/InscricaoController.php
+++ app/Http/Controllers/InscricaoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Inscricao;
 
 class InscricaoController extends Controller
 {
--- app/Http/Controllers/VagaController.php
+++ app/Http/Controllers/VagaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Vaga;
 
 class VagaController extends Controller
 {
