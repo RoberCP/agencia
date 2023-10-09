9ae54c8dc8ebca48354f014ee0c6de59581c5b51
Natalia Mello <rosbakmello@gmail.com>
2023-10-09 12:06:40 -0300

    controllers corrigidos

--- app/Http/Controllers/CandidatoController.php
+++ app/Http/Controllers/CandidatoController.php
@@ -10,7 +10,7 @@ class CandidatoController extends Controller
     // Mostra uma visão geral dos registros
     public function index()
     {
-        // Obtém todos os candidatos do banco de dados
+        // Obtém todas as Candidato do banco de dados
         $candidatos = Candidato::all();
         return view('candidatos.index', compact('candidatos'));
     }
@@ -24,10 +24,12 @@ class CandidatoController extends Controller
     // Recebe os dados do formulário e salva no banco de dados
     public function store(Request $request)
     {
+        $candidatos = Candidato::all();
+
         // Cria uma nova instância do model 'Candidato' com os dados fornecidos no request
         $candidato = new Candidato([
             'nome' => $request->input('nome'),
-            'dataNasc' => $request->input('dataNasc'),
+            'dtNasc' => $request->input('dtNasc'),
             'telefone' => $request->input('telefone'),
             'genero' => $request->input('genero'),
         ]);
@@ -36,33 +38,47 @@ class CandidatoController extends Controller
         return redirect()->route('candidatos.index');
     }
 
-
+    // Exibe um registro específico
     public function show(string $id)
     {
-        //
+        // Encontra uma Candidato no banco de dados com o ID fornecido
+        $candidato = Candidato::findOrFail($id);
+        // Retorna a view 'Candidatos.show' e passa a Candidato como parâmetro
+        return view('candidatos.show', compact('candidato'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    // Formulário de edição de um registro
     public function edit(string $id)
     {
-        //
+        // Encontra uma Candidato no banco de dados com o ID fornecido
+        $candidato = Candidato::findOrFail($id);
+        // Retorna a view 'Candidatos.edit' e passa a Candidato como parâmetro
+        return view('candidatos.edit', compact('candidato'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    // Recebe os dados do formulário de edição e atualiza no banco de dados
     public function update(Request $request, string $id)
     {
-        //
+        $candidato = Candidato::findOrFail($id);
+        // Atualiza os campos da Candidato com os dados fornecidos no request
+        $candidato->id = $request->input('id');
+        $candidato->nome = $request->input('nome');
+        $candidato-> dtNasc = $request->input('dtNasc');
+        $candidato-> telefone = $request->input('telefone');
+        $candidato->genero = $request->input('genero');
+        // Salva as alterações
+        $candidato->save();
+        // Redireciona para a rota 'Candidatos.index' após salvar
+        return redirect()->route('candidatos.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    // Exclui um registro
     public function destroy(string $id)
     {
-        //
+        $candidato = Candidato::findOrFail($id);
+        // Exclui a Candidato do banco de dados
+        $candidato->delete();
+        // Redireciona para a rota 'Candidatos.index' após salvar
+        return redirect()->route('candidatos.index');
     }
 }
--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -7,59 +7,74 @@ use App\Models\Empresa;
 
 class EmpresaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    // Mostra uma visão geral dos registros
     public function index()
     {
-        //
+        // Obtém todas as empresa do banco de dados
+        $empresas = Empresa::all();
+        return view('empresas.index', compact('empresas'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    // Formulário para criar um novo registro
     public function create()
     {
-        //
+        return view('empresas.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    // Recebe os dados do formulário e salva no banco de dados
     public function store(Request $request)
     {
-        //
+        $empresas = Empresa::all();
+        
+        // Cria uma nova instância do model 'empresa' com os dados fornecidos no request
+        $empresa = new Empresa([
+            'nome' => $request->input('nome'),
+            'uf' => $request->input('uf')
+        ]);
+        // Salva no banco de dados
+        $empresa->save();
+        return redirect()->route('empresas.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
+    // Exibe um registro específico
     public function show(string $id)
     {
-        //
+        // Encontra uma empresa no banco de dados com o ID fornecido
+        $empresa = Empresa::findOrFail($id);
+        // Retorna a view 'empresas.show' e passa a empresa como parâmetro
+        return view('empresas.show', compact('empresa'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    // Formulário de edição de um registro
     public function edit(string $id)
     {
-        //
+        // Encontra uma empresa no banco de dados com o ID fornecido
+        $empresa = Empresa::findOrFail($id);
+        // Retorna a view 'empresas.edit' e passa a empresa como parâmetro
+        return view('empresas.edit', compact('empresa'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    // Recebe os dados do formulário de edição e atualiza no banco de dados
     public function update(Request $request, string $id)
     {
-        //
+        $empresa = Empresa::findOrFail($id);
+        // Atualiza os campos da empresa com os dados fornecidos no request
+        $empresa->id = $request->input('id');
+        $empresa->nome = $request->input('nome');
+        $empresa->uf = $request->input('uf');
+        // Salva as alterações
+        $empresa->save();
+        // Redireciona para a rota 'empresas.index' após salvar
+        return redirect()->route('empresas.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    // Exclui um registro
     public function destroy(string $id)
     {
-        //
+        $empresa = Empresa::findOrFail($id);
+        // Exclui a empresa do banco de dados
+        $empresa->delete();
+        // Redireciona para a rota 'empresas.index' após salvar
+        return redirect()->route('empresas.index');
     }
 }
