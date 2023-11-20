e6fc1af01f6e1b7a8165cb9b377228dc993052a0
Roberta Parisotto <robertaparisotto.parisotto2@gmail.com>
2023-11-20 20:16:50 -0300

    show candidatos

--- resources/views/candidatos/show.blade.php
+++ resources/views/candidatos/show.blade.php
@@ -3,11 +3,13 @@
         <link rel="stylesheet" href="{{ asset('css/candidatos.css') }}">
         <h1>Detalhes do Candidato</h1>
         <ul>
-            <li><strong>ID:</strong> {{ $candidato->cpf }}</li>
-            <li><strong>Nome:</strong> {{ $autor->nome }}</li>
-            <li><strong>Nacionalidade:</strong> {{  $autor->nacionalidade }}</li>
-            <li><strong>Data de Nascimento:</strong> {{ date_format(new DateTime($autor->data_nascimento), 'd/m/Y') }}</li>
+            <li><strong>CPF:</strong> {{ $candidato->cpf }}</li>
+            <li><strong>Nome:</strong> {{ $candidato->nome }}</li>
+            <li><strong>Data de Nascimento:</strong> {{ date_format(new DateTime($candidato->data_nascimento), 'd/m/Y') }}</li>
+            <li><strong>Telefone:</strong> {{  $candidato->telefone }}</li>
+            <li><strong>Gênero:</strong> {{  $candidato->genero }}</li>
+            <li><strong>Cidade:</strong> {{  $candidato->cidade }}</li>
         </ul>
-        <a href="{{ route('autores.index') }}" class="btn btn-secondary">Voltar</a>
+        <a href="{{ route('candidatos.index') }}" class="btn btn-secondary">Voltar</a>
     </div>
 </x-app-layout>
